e2d90b9273379419c254020d7023cf094e7b7940
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-01-21 20:31:53 -0300

    Adding NotFound style page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { StyledContainer } from './styles';
+import GifPikachuCrying from '../../assets/gif/pikachu-crying.gif';
 
 export function NotFound() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 3000);
+  }, []);
+  return (
+    <StyledContainer>
+      <h1>Page not found, you will be redirected...</h1>
+      <img src={GifPikachuCrying} alt="Crying Pikachu" loading="lazy" />
 
-
-    useEffect(() => {
-
-        setTimeout(() => {
-        navigate("/")
-        }, 3000)
-    }, [])
-    return (<h1>Page not found, you will be redirected...</h1>)
+    </StyledContainer>
+  );
 }
---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,16 @@
+import { styled } from "@mui/material";
+
+export const StyledContainer = styled("div")({
+    display: 'flex', 
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: "column",
+    gap: "1rem",
+    minHeight: "100vh",
+
+
+    ">img": {
+        width: "100px",
+        height: "100px"
+    }
+})
